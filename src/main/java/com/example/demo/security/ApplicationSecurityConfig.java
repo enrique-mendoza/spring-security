912fc0f89f62a0c3d4a01579544acd2871a8bbbb
Enrique Mendoza <luis.mendoza@infotec.mx>
2025-04-08 20:17:58 -0600

    feat: request matchers

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -7,21 +7,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * WebSecurityConfigurerAdapter was deprecated in Spring Security 5.7.0.
+ * Read about it:
+ * <a href="https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter">
+ *     Spring Security without the WebSecurityConfigurerAdapter
+ * </a>
+ */
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
 
-    // WebSecurityConfigurerAdapter was deprecated in Spring Security 5.7.0.
-    // Read about it: https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                (
-                        authz -> authz
-                                .anyRequest()
-                                .authenticated()
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/", "/index", "/index.html", "/css/*", "/js/*").permitAll()
+                        .anyRequest()
+                        .authenticated()
                 )
-        ).httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
