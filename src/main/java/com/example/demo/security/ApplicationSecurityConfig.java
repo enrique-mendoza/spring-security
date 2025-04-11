a2059ba9dc96ae54b972b5aec2c2a87be7c89c07
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-04-10 18:41:54 -0600

    feat: create in memory user

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -29,5 +32,16 @@ public class ApplicationSecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password("password")
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(annaSmithUser);
     }
 }
