40513b79ad421aa8b163e60802d663b39f1b99ec
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-04-14 19:28:33 -0600

    feat(roles): admin trainee

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static com.example.demo.security.ApplicationUserRole.ADMIN;
+import static com.example.demo.security.ApplicationUserRole.ADMINTRAINEE;
 import static com.example.demo.security.ApplicationUserRole.STUDENT;
 
 /**
@@ -61,6 +62,12 @@ public class ApplicationSecurityConfig {
                 .roles(ADMIN.name()) // ROLE_ADMIN
                 .build();
 
-        return new InMemoryUserDetailsManager(student, admin);
+        UserDetails adminTrainee = User.builder()
+                .username("admin_trainee")
+                .password(passwordEncoder.encode("admintrainee123"))
+                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+                .build();
+
+        return new InMemoryUserDetailsManager(student, admin, adminTrainee);
     }
 }
